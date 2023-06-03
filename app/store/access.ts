9d284dc785beb538d0992ebfdefaf253eca5bd4c
dragonsea0 <71763561+dragonsea0@users.noreply.github.com>
2023-06-03 20:08:20 +0800

    Update access.ts

--- app/store/access.ts
+++ app/store/access.ts
@@ -25,7 +25,7 @@ let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 export const useAccessStore = create<AccessControlStore>()(
   persist(
     (set, get) => ({
-      token: "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",
+      token: "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",
       accessCode: "",
       needCode: true,
       hideUserApiKey: false,
